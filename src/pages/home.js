592eb6afea0e1d95c46e4a670dd70c123f49f02c
yorblesnoot <ethereos@gmail.com>
2024-09-25 23:31:52 -0400

    reworked project page

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,18 +6,21 @@ function HomePage()
     return(
       <div className='frame'>
         
-        <div className='block' style={{justifyContent:'center'}}>
-          <img src = {selfie} alt='ED' width="40%" height="40%"/>
+        <div className='block' style={{justifyContent:'center', line: '0'}}>
+          <img src = {selfie} alt='ED' width="30%" height="30%"/>
             
         </div>
-        <div className='block' style={{justifyContent:'center'}}>
+        <div className='block' style={{marginLeft: '10%', marginRight: '10%'}}>
           <div className='innerblock' style={{justifyContent:'center'}}>
             <div className='med-word'>
-              Hi! My name is <span className='big-word'>Eduardo "Ed" Korb</span> and I'm a Unity and React Developer. 
+            Hi! My name is <span className='big-word'>Eduardo Korb</span>, and I'm a Game and Web Developer! 
+            </div>
+            <div className='thin-column'>
+              I'm particularly interested in Unity, React, procedural generation, pathfinding algorithms, 3D modeling, and VFX; and I like to incorporate all those elements into my game development!
             </div>
             <br></br>
-            <div className='thin-column'>Before that I was a technical writer, editor, and tutor in algebra, calculus, physics, geometry/, SAT, GRE, english,spanish and logic.
-            I love to teach, and I love to learn.
+            <div className='thin-column'>Before rebooting my career as a software developer, I was a technical writer, editor, and tutor in algebra, calculus, physics, geometry, SAT, GRE, english, spanish, and logic.
+            I love to teach and I love to learn.
               If you're looking for a quick study, a deep and eclectic pool of knowledge, and a touch of whimsy—I'm your guy!</div>
           </div>
         </div>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,26 +1,33 @@
 import './page.css';
 import slime from '../assets/slimemini.gif'
-import robot from '../assets/botlegs.gif'
+import robot from '../assets/pathing.gif'
 import lantern from '../assets/lflies.png'
 import site from '../assets/site.png'
-import tree from '../assets/tree.png'
+import tree from '../assets/trees.gif'
+import ctq from '../assets/ctq.gif'
 
 function ProjectsPage()
 {
   return(
     <div  className='frame'>
-        <Project title='Tiny Robot Tactics' imageurl={robot} 
-        blurb="A micro-CRPG I am currently working on in Unity. It emulates the combat feel of Baldur's Gate 3 while extrapolating that gameplay into the third dimension and replacing all the characters with little robots. Makes extensive use of procedural animation to bring modular characters to life."/>
-        <Project title='Prototype Tree Generator' imageurl={tree} 
-        blurb="What started as a potential level idea for Tiny Robot Tactics became a week-long quest to create the perfect procedural tree generator, built based on a research paper I found online. Uses Djikstra's algorithm and guiding vectors to create natural-looking branch patterns. It ultimately didn't seem conducive to compelling gameplay, so it's currently sitting in disuse. It did, however, produce some visually interesting specimens."/>
-        <Project title="Don, Lanternflies, N'at" imageurl={lantern} link="https://globalgamejam.org/games/2024/don-lanternflies-nat-8"
-        blurb="For the 2024 Global Game Jam, I joined an already-established team with a clear vision. Deviating from my typical coding focus, I modeled and rigged a spotted lanternfly for the team in 24 hours. Then, I helped the team debug their Unity code and complete their game. Download the finished product "/>
-        <Project title='Slime Alchemist' imageurl={slime} link="https://store.steampowered.com/app/2567800/Slime_Alchemist/" 
-        blurb="My first game, a roguelike deckbuilder I began developing while enrolled in web development bootcamp. Several months after finishing bootcamp, I released the game into Early Access on Steam. Although I'm proud of what I learned and accomplished making Slime Alchemist, my focus is now on sustainably finalizing the project to move onto better conceived works. Visit the steam page  "/>
-        <Project title='yorblesnoot.github.io' imageurl={site} 
-        blurb="You guessed it, it's this exact website! I made it in React to hone my js/html/css skills. I never claimed design was my strong suit, but I did make a dope navigation bar that I'm proud of."/>
-        <Project title='MineSweeper' imageurl='https://img-9gag-fun.9cache.com/photo/av8WwvE_460s.jpg' 
-        blurb="A clone of the popular(?) game MineSweeper for the Windows CLI, which I put together over the course of an 8-hour day of web development bootcamp instead of doing our actual assignment. The coolest part of it was writing the logic to reveal nearby squares when you reveal a square with no mines around it; that took some braining to figure out."/>
+        <div className='projectholder'>
+            <div className='projectcolumn' style={{alignSelf: 'right'}}>
+                <Project title='Tiny Robot Tactics' imageurl={robot} 
+                blurb="A micro-CRPG I am currently working on in Unity. It emulates the combat feel of Baldur's Gate 3 while extrapolating that gameplay into the third dimension and replacing all the characters with little robots. Uses a Breadth-First Search to allow units to pathfind in a fully 3-D space, and procedural animation with inverse kinematics to animate their bodies."/>
+                <Project title='Prototype Tree Generator' imageurl={tree} 
+                blurb="What started as a potential level idea for Tiny Robot Tactics became a week-long quest to create the perfect procedural tree generator, built based on a research paper I found online. Uses Djikstra's algorithm and guiding vectors to create natural-looking branch patterns. It ultimately didn't seem conducive to compelling gameplay, so it's currently sitting in disuse. It did, however, produce some visually interesting specimens."/>
+                <Project title="Don, Lanternflies, N'at" imageurl={lantern} link="https://globalgamejam.org/games/2024/don-lanternflies-nat-8"
+                blurb="For the 2024 Global Game Jam, I joined an already-established team with a clear vision. Deviating from my typical coding focus, I modeled and rigged a spotted lanternfly for the team in 24 hours. Then, I helped the team debug their Unity code and complete their game. Download the finished product "/>
+            </div>
+            <div className='projectcolumn' style={{alignSelf: 'left'}}>
+                <Project title='Slime Alchemist' imageurl={slime} link="https://store.steampowered.com/app/2567800/Slime_Alchemist/" 
+                blurb="A roguelike deckbuilder I began developing while enrolled in web development bootcamp. Several months after finishing bootcamp, I released the game into Early Access on Steam. I designed the gameplay and visual effects, wrote the entirety of the game's code, and built the environments from store-bought assets. Visit the steam page  "/>
+                <Project title='yorblesnoot.github.io' imageurl={site} 
+                blurb="You guessed it, it's this exact website! I built it in react to serve as a simple online portfolio for my development work."/>
+                <Project title='Care Team Quest' imageurl={ctq} 
+                blurb='A Pacman clone and "medical education experience" I made in Unity while working for Brandgage. I programmed the entirety of the game logic and worked alongside a designer to bring the game to life.'/>
+            </div>
+        </div>
     </div>
   )
 }
@@ -34,17 +41,20 @@ function Project({title, imageurl, blurb, link})
     }
     return(
         <div className='projectdisplay'>
-            <div className='projectimage'>
-                <img src={imageurl} alt='project' width={300} height={300}/>
+            <div className='flip-display'>
+                <div className='flip-front'>
+                    <img src={imageurl} alt='project'/>
+                </div>
+                <div className='flip-back'>
+                    <div className='back-text'>
+                        <h1>{title}</h1>
+                        {blurb}
+                        <a href={link}>{linkblurb}</a>
+                        <div style={{paddingTop: '50px'}}></div>
+                    </div>
+                    
+                </div>
             </div>
-            
-            <div className='projectwords'>
-                <h1>{title}</h1>
-                {blurb}
-                <a href={link}>{linkblurb}</a>
-                <div style={{paddingTop: '50px'}}></div>
-            </div>
-
         </div>
     )
 }
